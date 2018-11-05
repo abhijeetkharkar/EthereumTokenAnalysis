ebd54e3a1a0b0d262339344c9a6ac672f9403423
abhijeetkharkar <abhijeetkharkar@gmail.com>
2018-11-04 21:45:05 -0600

    Storing everything except transactions in a dictionary and setting this dictionary as the value of the key block number in another dictionary. Using JSON Parser again. Fixing the while loop condition error ('is not' should not be used).

--- extractors/extract_block_timestamps.py
+++ extractors/extract_block_timestamps.py
@@ -1,16 +1,17 @@
 import urllib.request
 import json
 from config_files import config
-import pickle
 from datetime import datetime
 import multiprocessing
 
 
 def get_block_details(block_tuple):
-    print("Block Tuple: (", multiprocessing.current_process().name, "):", block_tuple)
+    print("Block Tuple(", multiprocessing.current_process().name, "):", block_tuple)
     earliest_block = block_tuple[0]
+    # earliest_block = 3788557
     current_block = earliest_block
     latest_block = block_tuple[1]
+    # latest_block = 3788600
 
     block_timestamps_map = dict()
 
@@ -18,7 +19,7 @@ def get_block_details(block_tuple):
         data = json.load(json_data)
 
     # for block_number in range(earliest_block, latest_block+1):
-    while current_block is not latest_block:
+    while current_block != latest_block:
         req = urllib.request.Request(config.CONFIGURATION['DEFAULT']['GETH_URL'])
         req.add_header('Content-Type', 'application/json; charset=utf-8')
 
@@ -27,26 +28,52 @@ def get_block_details(block_tuple):
         response_str = "**Default**"
 
         try:
-            response = urllib.request.urlopen(req, json.dumps(data).encode("utf-8"))
+            response = urllib.request.urlopen(req, json.dumps(data).encode("utf-8")).read()
+            block_details = json.loads(response.strip())["result"]
 
-            # print("Response:", response.read())
+            # if current_block == 3788570:
+            #     # print("Response:", response)
+            #     print(block_details.keys())
+            #     print(block_details['logsBloom'])
 
-            # block_details = json.loads(response.read().strip())["result"]
-            # block_timestamps_map[current_block] = datetime.fromtimestamp(int(block_details["timestamp"].strip())/1000.0)
+            block_data = dict()
+            block_data['difficulty'] = block_details["difficulty"].strip()
+            block_data['extraData'] = block_details["extraData"].strip()
+            block_data['gasLimit'] = block_details["gasLimit"].strip()
+            block_data['gasUsed'] = block_details["gasUsed"].strip()
+            block_data['hash'] = block_details["hash"].strip()
+            block_data['miner'] = block_details["miner"].strip()
+            block_data['mixHash'] = block_details["mixHash"].strip()
+            block_data['nonce'] = block_details["nonce"].strip()
+            block_data['number'] = block_details["number"].strip()
+            block_data['parentHash'] = block_details["parentHash"].strip()
+            block_data['receiptsRoot'] = block_details["receiptsRoot"].strip()
+            block_data['size'] = block_details["size"].strip()
+            block_data['stateRoot'] = block_details["stateRoot"].strip()
+            block_data['timestamp'] = block_details["timestamp"].strip()
+            block_data['totalDifficulty'] = block_details["totalDifficulty"].strip()
+            block_data['transactionsRoot'] = block_details["transactionsRoot"].strip()
+            # print(datetime.fromtimestamp(int(block_details["timestamp"].strip(), 16)))
 
-            response_str = str(response.read())
+            block_timestamps_map[current_block] = block_data
+
+            # response_str = str(response)
             # print(response_str)
             # timestamp = int(response_str[response_str.find('"timestamp"') + 13:][0:response_str[response_str.find('"timestamp"') + 13:].find('"')], 16)
-            # block_timestamps_map[current_block] = datetime.fromtimestamp(timestamp / 1000.0)
-            block_timestamps_map[current_block] = response_str
+            # block_timestamps_map[current_block] = datetime.fromtimestamp(timestamp)
+            # block_timestamps_map[current_block] = response_str
 
             current_block += 1
+            # print("Good Block:", current_block)
             if current_block % 100000 == 0:
                 print("Progress (", multiprocessing.current_process().name, "):", round((current_block - earliest_block) / (latest_block - earliest_block) * 100, 4))
-        except ValueError:
-            print("Erroneous Block:", current_block)
+        except ValueError as e:
+            print("Erroneous Block:", current_block, "******** Exception: ", e)
             block_timestamps_map[current_block] = response_str
+            current_block += 1
         except ConnectionError:
             pass
+
+    # print(block_timestamps_map)
 
     return block_timestamps_map
