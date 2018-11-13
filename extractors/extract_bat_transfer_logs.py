1d3b28c0783994a553d8761c33941f55099acb6f
abhijeetkharkar <abhijeetkharkar@gmail.com>
2018-11-13 03:11:33 -0600

    Storing all the fields in pickle object

--- extractors/extract_bat_transfer_logs.py
+++ extractors/extract_bat_transfer_logs.py
@@ -2,17 +2,13 @@ import urllib.request
 import json
 from utilities import config
 import pickle
-import time
-import sys
-import os
+from datetime import datetime
 
 
 def get_bat_transfer_logs():
-    earliest_block = 3788557
-    # events_csv = open("output/bat_transfer_events.csv", "w")
+    print("Extraction of BAT Transfer Logs started", str(datetime.today()))
 
-    # print("To,From,Data,Block Number,Transaction Hash,Transaction Index,Block Hash,Log Index")
-    # events_csv.write("To,From,Data,Block Number,Transaction Hash,Transaction Index,Block Hash,Log Index\n")
+    earliest_block = 3788557
     events_list = []
     counter = 1
     index_after_last_pass = 0
@@ -23,16 +19,11 @@ def get_bat_transfer_logs():
         with open('json_requests/get_bat_transfer_logs.json') as json_data:
             data = json.load(json_data)
 
-        # print(data)
-        # print(data["params"][0]["fromBlock"])
-
         data["params"][0]["fromBlock"] = hex(earliest_block)
         data["params"][0]["toBlock"] = hex(earliest_block + 50000)
 
-        # print(data)
         try:
             response = urllib.request.urlopen(req, json.dumps(data).encode("utf-8"))
-
             events_log = json.loads(response.read())["result"]
 
             print("Pass", counter, ", #Events:", len(events_log), "(Block -", earliest_block, "TO Block -", earliest_block+50000, ")")
@@ -41,37 +32,21 @@ def get_bat_transfer_logs():
                 break
 
             for events in events_log:
-                row = ""
                 event_data = {}
                 for key in events.keys():
-                    if key.strip() != "removed":
-                        event_data[key.strip()] = events.get(key)
-
-                    # if key.strip() == "topics":
-                    #     row += events.get(key)[1].strip() + ","
-                    #     event_data[key.strip()] = events.get(key)[1].strip()
-                    # elif key.strip() == "removed":
-                    #     pass
-                    # else:
-                    #     # print(key.strip())
-                    #     if key.strip() == "data":
-                    #         row += str(int(events.get(key), 16) / 10**18) + ","
-                    #         event_data[key.strip()] = int(events.get(key), 16) / 10**18
-                    #     else:
-                    #         row += events.get(key).strip() + ","
-                    #         event_data[key.strip()] = events.get(key)[1].strip()
-                # row = row[:-1]
-                # print(row)
+                    event_data[key.strip()] = events.get(key)
                 events_list.append(event_data)
-                # events_csv.write(row+"\n")
 
             earliest_block += 50001
             counter += 1
             index_after_last_pass = len(events_list)
         except ConnectionError:
             events_list = events_list[0:index_after_last_pass]
-    print("Total Transactions:", len(events_list), ", Size(in MB):", sys.getsizeof(events_list) / 1024.0 / 1024.0)
-    os.system("vmstat")
+
+    print("Total Transactions:", len(events_list))
+    # os.system("vmstat")
     # time.sleep(120)
     bat_transfer_events_dump = open("pickled_objects/bat_transfer_events_list", "wb")
     pickle.dump(events_list, bat_transfer_events_dump)
+
+    print("Extraction of BAT Transfer Logs ended", str(datetime.today()))
