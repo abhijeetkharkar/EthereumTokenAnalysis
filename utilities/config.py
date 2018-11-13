3d1fffd28ad80a0a7316331c32ee1e486afbf7d5
abhijeetkharkar <abhijeetkharkar@gmail.com>
2018-11-13 02:10:44 -0600

    Renaming package from "config_files" to "utilities"

--- utilities/config.py
+++ utilities/config.py
@@ -4,4 +4,4 @@ import configparser
 def init():
     global CONFIGURATION
     CONFIGURATION = configparser.ConfigParser()
-    CONFIGURATION.read('config_files/general_config.ini')
+    CONFIGURATION.read('utilities/general_config.ini')
